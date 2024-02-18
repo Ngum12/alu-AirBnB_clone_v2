898b58156e5437ea2f9bd5afe93db95d52558f78
Ngum12 <d.ngum@alustudent.com>
2024-02-18 10:23:14 -0800

    state in py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application,
-Web application is listening on 0.0.0.0, port 5000 """
+"""
+start Flask application
+"""
 
-from models import storage
-from models.state import State
 from flask import Flask, render_template
-from flask import abort
-
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def state_list():
-    """state_list"""
-    states = storage.all(State).values()
-    return render_template(
-        "9-states.html",
-        states=states,
-        condition="states_list")
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def states_by_id(id):
-    """states_by_id"""
-    all_states = storage.all(State)
-    state = all_states.get(id)
-    if state is None:
-        abort(404)
-    return render_template(
-        '9-states.html',
-        state=state,
-        condition="state_id")
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Removes the current SQLAlchemy Session"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
+    app.run(host='0.0.0.0', port='5000')
