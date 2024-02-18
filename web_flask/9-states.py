3de25b74917a169d224daba1d4ba52040f6bd7c2
Ngum12 <d.ngum@alustudent.com>
2024-02-18 10:18:17 -0800

    state in py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,34 +1,44 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
-from flask import Flask, render_template
+""" Write a script that starts a Flask web application,
+Web application is listening on 0.0.0.0, port 5000 """
+
 from models import storage
 from models.state import State
-
+from flask import Flask, render_template
+from flask import abort
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close(self):
-    """ Method to close the session """
-    storage.close()
-
-
 @app.route('/states', strict_slashes=False)
-def state():
-    """Displays a html page with states"""
-    states = storage.all(State)
-    return render_template('9-states.html', states=states, mode='all')
+def state_list():
+    """state_list"""
+    states = storage.all(State).values()
+    return render_template(
+        "9-states.html",
+        states=states,
+        condition="states_list")
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def state_by_id(id):
-    """Displays a html page with citys of that state"""
-    for state in storage.all(State).values():
-        if state.id == id:
-            return render_template('9-states.html', states=state, mode='id')
-    return render_template('9-states.html', states=state, mode='none')
+def states_by_id(id):
+    """states_by_id"""
+    all_states = storage.all(State)
+    state = all_states.get(id)
+    if state is None:
+        abort(404)
+    return render_template(
+        '9-states.html',
+        state=state,
+        condition="state_id")
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host='0.0.0.0')
+
