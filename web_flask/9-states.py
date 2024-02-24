1017e61485cd3daacff5e7d9eae2a037dce21515
Ngum12 <d.ngum@alustudent.com>
2024-02-24 01:20:28 -0800

    set up and fixed

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
-
-from flask import Flask, render_template, abort
+"""Importing Flask to run the web app"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
-def close_storage(exception):
+def close(self):
+    """ Method to close the session """
     storage.close()
 
+
 @app.route('/states', strict_slashes=False)
-def get_states():
-    try:
-        states = storage.all(State)
-        return render_template('9-states.html', states=states, mode='all')
-    except Exception as e:
-        app.logger.error('An error occurred: {}'.format(str(e)))
-        abort(500)
+def state():
+    """Displays a html page with states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
+
 
 @app.route('/states/<id>', strict_slashes=False)
-def get_state_by_id(id):
-    try:
-        state = storage.get(State, id)
-        if state:
+def state_by_id(id):
+    """Displays a html page with citys of that state"""
+    for state in storage.all(State).values():
+        if state.id == id:
             return render_template('9-states.html', states=state, mode='id')
-        else:
-            abort(404)
-    except Exception as e:
-        app.logger.error('An error occurred: {}'.format(str(e)))
-        abort(500)
+    return render_template('9-states.html', states=state, mode='none')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host="0.0.0.0", port="5000")
