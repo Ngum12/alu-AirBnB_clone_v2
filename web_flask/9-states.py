7cdb7ff5ad46ba601270df27343fb359da228ac8
Ngum12 <d.ngum@alustudent.com>
2024-02-24 07:39:03 -0800

    fixing all

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Closes the session"""
+def close(self):
+    """ Method to close the session """
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
-def states():
-    """ display a HTML page: (inside the tag BODY) """
-    states = sorted(list(storage.all(State).values()),
-                    key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
+def state():
+    """Displays a html page with states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def states_id(id):
-    """ display a HTML page: (inside the tag BODY) """
-    states = storage.all(State, id)
-    if states:
-        cities = sorted(states.cities, key=lambda city: city.name)
-        return render_template('9-states.html', state=states, cities=cities)
-    else:
-        return render_template('9-states.html', state=None, cities=None)
+def state_by_id(id):
+    """Displays a html page with citys of that state"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
